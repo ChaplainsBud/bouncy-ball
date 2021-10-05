9cbf42920f13985d10dd41fe66bd9b161b2d0841
Matt Leroe <matthew.robert.leroe@gmail.com>
2021-10-05 18:33:59 -0400

    Initial Commit

---
+++ bouncy-ball/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  bouncy-ball
+//
+//  Created by Matthew Leroe on 10/5/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
